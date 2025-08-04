31e6a735122675d726843dbd8fa71536c189e5ac
cwal8202 <cwal8202@naver.com>
2025-08-04 18:01:10 +0900

    web 화면 구현중

---
+++ web_service/Chill_Tuna/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Chill_Tuna project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Chill_Tuna.settings")
+
+application = get_asgi_application()
