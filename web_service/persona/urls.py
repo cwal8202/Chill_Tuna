a48894eb699ab0deb5fbbecfd326030330cf8713
onizuka-38 <bdh9938@gmail.com>
2025-08-07 15:31:35 +0900

    persona폴더

--- web_service/persona/urls.py
+++ web_service/persona/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 app_name = "persona"
 urlpatterns = [
-    # path('', views.home, name='home'),  # /persona/
+    path('', views.create_persona, name='create'),  # /persona/
 ]
