9b9e7cf81e03230f9c02f46156c175c29e758ac0
jsa3338 <heungnam3338@naver.com>
2025-08-07 15:18:29 +0900

    models.py 작성완료

--- web_service/persona/models.py
+++ web_service/persona/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Persona(models.Model):
+    id = models.IntegerField(auto_created=True, primary_key=True)
+    name = models.CharField(max_length=20)
+    segment = models.CharField(max_length=50)
+    age_group = models.CharField(max_length=10)
+    family_structure = models.CharField(max_length=50)
+    gender = models.CharField(max_length=10)
+    customer_value = models.CharField(max_length=50)
+    purchase_pattern = models.JSONField(max_length=100)
+    lifestyle = models.JSONField(max_length=100)
+    job = models.CharField(max_length=50)
+    persona_summary_tag = models.TextField(max_length=500)
+
+    def __str__(self):
+        # 객체를 문자열로 표현할 때 사용합니다.
+        # 예: '30대 여성 웰빙형'
+        # return f"{self.age_group} {self.gender} {self.lifestyle} 페르소나"
+        return f"{self.segment} - 페르소나 {self.id} {self.name}"
+
+    class Meta:
+        verbose_name_plural = "페르소나" # Django 관리자 페이지에서 보여지는 이름
